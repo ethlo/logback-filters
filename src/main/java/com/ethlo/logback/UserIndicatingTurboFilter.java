2a63440741e94877d6bf440890eef899287d34ed
Morten <github@ethlo.com>
2013-04-05 08:36:55 +0000

    Adding support for extra properties in user data

--- src/main/java/com/ethlo/logback/UserIndicatingTurboFilter.java
+++ src/main/java/com/ethlo/logback/UserIndicatingTurboFilter.java
@@ -1,5 +1,13 @@
 package com.ethlo.logback;
 
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 import org.slf4j.Marker;
 
@@ -17,10 +25,63 @@ import com.ethlo.util.spring.security.SpringAuthenticationFetcher;
  */
 public class UserIndicatingTurboFilter extends TurboFilter
 {
+	private final org.slf4j.Logger systemLogger = LoggerFactory.getLogger(UserIndicatingTurboFilter.class);
+	private String prefix = "";
+	private String[] extraPropertyNames;
+
 	@Override
 	public FilterReply decide(Marker marker, Logger logger, Level level, String format, Object[] params, Throwable t)
 	{
 		MDC.put("username", SpringAuthenticationFetcher.isAvailable() ? SpringAuthenticationFetcher.getUsername() : "?");
+		
+		if (SpringAuthenticationFetcher.isAvailable() && extraPropertyNames != null)
+		{
+			final Object auth = SpringAuthenticationFetcher.getAuthentication(Object.class);
+			if (auth != null)
+			{
+				for (String extraPropertyName : extraPropertyNames)
+				{
+					try
+					{
+						final BeanInfo info = Introspector.getBeanInfo(auth.getClass());
+						final PropertyDescriptor[] pds = info.getPropertyDescriptors();
+						final Method m = findMethod(extraPropertyName, pds);
+						if (m != null)
+						{
+							final Object extraPropertyValue = m.invoke(auth, new Object[0]);
+							MDC.put(prefix + extraPropertyName, extraPropertyValue.toString());
+						}
+					}
+					catch (IntrospectionException | IllegalAccessException | IllegalArgumentException | InvocationTargetException exc)
+					{
+						systemLogger.warn("", exc);
+					}
+				}
+			}
+		}
+		
         return FilterReply.NEUTRAL;
+	}
+
+	private Method findMethod(String extraPropertyName, PropertyDescriptor[] pds)
+	{
+		for (PropertyDescriptor desc : pds)
+		{
+			if (desc.getName().equals(extraPropertyName))
+			{
+				return desc.getReadMethod();
+			}
+		}
+		return null;
+	}
+
+	public void setPrefix(String prefix)
+	{
+		this.prefix = prefix;
+	}
+
+	public void setExtraPropertyNames(String[] extraPropertyNames)
+	{
+		this.extraPropertyNames = extraPropertyNames;
 	}
 }
